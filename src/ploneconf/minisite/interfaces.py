3e28357cc33c9af8702552b750e2086ca275d51d
csanahuja <csanahuja10@gmail.com>
2016-12-13 11:05:09 +0100

    Added marker interface IMinisite and registered as behavior

--- src/ploneconf/minisite/interfaces.py
+++ src/ploneconf/minisite/interfaces.py
@@ -2,7 +2,11 @@
 """Module where all interfaces, events and exceptions live."""
 
 from zope.publisher.interfaces.browser import IDefaultBrowserLayer
+from zope.interface import Interface
 
 
 class IPloneconfMinisiteLayer(IDefaultBrowserLayer):
     """Marker interface that defines a browser layer."""
+
+class IMinisite(Interface):
+    """Maker inerface for Minisite"""
