501b151c0b04323d546db3dea1d001565c844e88
csanahuja <csanahuja10@gmail.com>
2016-12-15 10:05:18 +0100

    Now viewlet logo of minisite it is also displayed in childs

--- src/ploneconf/minisite/browser/viewlets.py
+++ src/ploneconf/minisite/browser/viewlets.py
@@ -1,9 +1,32 @@
 from plone.app.layout.viewlets.common import ViewletBase
+from Products.CMFCore.interfaces import ISiteRoot
 from plone import api
 
 class LogoViewlet(ViewletBase):
 
-    def image(self):
-        brains = api.content.find(self.context, portal_type='minisite')  
-        for brain in brains:      
-            return brain.getURL() + "/@@images/logo"
+    def image(self, parent=None):
+
+        chain = list(self.getAcquisitionChain(self.context))
+        for item in chain:
+            if (item.portal_type == 'minisite'):
+                return item.absolute_url() + "/@@images/logo"
+        return None
+
+
+    def getAcquisitionChain(self, object):
+
+        inner = object.aq_inner
+
+        iter = inner
+
+        while iter is not None:
+            yield iter
+
+            if ISiteRoot.providedBy(iter):
+               break
+
+            if not hasattr(iter, "aq_parent"):
+                raise RuntimeError("Parent traversing interrupted by object: " + str(parent))
+
+            iter = iter.aq_parent
+
