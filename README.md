15baa18b42c88af6f83920f5fecb577b2a16f154
csanahuja <csanahuja10@gmail.com>
2016-12-15 12:34:04 +0100

    Reworked how viewlet gets the logo image, now based on INavigationRoot

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # ploneconf.minisite
 Plone addon
+
+This plone addon allows you to create minisites creating a Dexterity CT:
+
+Functionalities:
+    -Set a custom logo to your minisite
+    -Create two default folders named News and Events
+    -Default view showing news inside the minisite (depth=2)
--- src/ploneconf/minisite/browser/viewlets.py
+++ src/ploneconf/minisite/browser/viewlets.py
@@ -5,28 +5,5 @@ from plone import api
 class LogoViewlet(ViewletBase):
 
     def image(self, parent=None):
-
-        chain = list(self.getAcquisitionChain(self.context))
-        for item in chain:
-            if (item.portal_type == 'minisite'):
-                return item.absolute_url() + "/@@images/logo"
-        return None
-
-
-    def getAcquisitionChain(self, object):
-
-        inner = object.aq_inner
-
-        iter = inner
-
-        while iter is not None:
-            yield iter
-
-            if ISiteRoot.providedBy(iter):
-               break
-
-            if not hasattr(iter, "aq_parent"):
-                raise RuntimeError("Parent traversing interrupted by object: " + str(parent))
-
-            iter = iter.aq_parent
+        return api.portal.get_navigation_root(self.context).absolute_url() + "/@@images/logo"
 
