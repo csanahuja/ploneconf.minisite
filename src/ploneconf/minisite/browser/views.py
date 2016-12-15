a355e932b0bc90540fa1df061181e661715aefef
csanahuja <csanahuja10@gmail.com>
2016-12-15 11:22:22 +0100

    Added default view to minisite to show news inside his folders news

--- src/ploneconf/minisite/browser/views.py
+++ src/ploneconf/minisite/browser/views.py
@@ -1,4 +1,22 @@
-from Products.Five.browser import BrowserView
 from plone import api
+from plone.dexterity.browser.view import DefaultView
+
+class MinisiteView(DefaultView):
+    """ View for minisite """
+
+    """ Get news in the folder minisite_id/news """
+    def news(self):
+        results = []
+
+        news_path = self.context.absolute_url() + "/news"
+        brains = api.content.find(self.context, depth=2,
+                                  portal_type="News Item", review_state="published")
+        for brain in brains:    
+            results.append({
+                'title': brain.Title,
+                'description': "nothing",
+                })
+
+        return results
 
 
